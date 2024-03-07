0f49a3830f2b2e5e48cd7b18bfb6636412869c0b
khushiv0811 <65448570+khushiv0811@users.noreply.github.com>
2024-03-07 11:25:17 -0500

    Initial Commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Kushboo Patel on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
